b7ae0e188ba4593d0154ce122903bdbd824634e7
sainttobs <sainttobs@gmail.com>
2021-06-11 03:05:47 +0100

    ficiic

--- python_notion_api/views.py
+++ python_notion_api/views.py
@@ -14,13 +14,15 @@ env = environ.Env()
 environ.Env.read_env()
 
 def index(request):
-    github_api = requests.get('https://api.github.com/repos/sainttobs/python_notion/issues')
-    github_api.headers={"Authorization": "token" + env('GITHUB_KEY'), "Accept": "application/vnd.github.v3+json"}
-    if github_api.status_code != 200:
+    github_data = requests.get('https://api.github.com/repos/sainttobs/python_notion/issues')
+    github_data.headers={"Authorization": "token" + env('GITHUB_KEY'), "Accept": "application/vnd.github.v3+json"}
+    if github_data.status_code != 200:
         raise ApiError('Response Status: {response.status_code}')
     else:
-        content = github_api.text
-        return HttpResponse(content)
+        # githubIssues = {}
+        # for data in github_data:
+        #     githubIssues['url'] = github_data[data]
+        return HttpResponse(githubIssuesurl)
 
 
 # Create your views here.
